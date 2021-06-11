8083e05b6f07371ff6b6712718578aa0a7331775
ogger77 <vud@kean.edu>
2021-06-11 17:00:32 -0400

    update start message

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -8,7 +8,7 @@ import { ExpirationCompleteListener } from "./events/listener/expiration-complet
 import { PaymentCreatedListener } from "./events/listener/payment-created-listener";
 
 const start = async () => {
-  console.log("Staring");
+  console.log("Staringaasasdasd");
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
